f35dff96a49e6fe6349d0321016baec0a02a07cd
Ishtiaq Ahmad <93281117+ishtiaqahmadbhatti@users.noreply.github.com>
2025-06-18 11:32:51 +0500

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,5 +1,5 @@
 def call(String image, String tag, String DockerHubUser){
    echo 'This is building the image.'
-   sh 'docker build -t ${DockerHubUser}/${image}:${tag} .'
+   sh 'docker build -t "${DockerHubUser}"/"${image}":"${tag}" .'
    echo 'Image Build And Tagged Successfully.'
 }
